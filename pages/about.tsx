a8223de87e42a20c0f7342aa9d4256f41cd288cf
Alessio Franceschi <alessiofranceschi2@gmail.com>
2022-07-26 21:40:43 +0200

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,12 +13,14 @@ export default function About(): JSX.Element {
 	/>
 	<h2>About</h2>
 	<p>
-		I spend most of my time programming and reading. Other than that, I&apos;m passionate about:
+		I spend most of my time programming or reading. Other than that, I&apos;m passionate about:
 	</p>
 		<ul>
+			<li>Gardening</li>
 			<li>Writing</li>
 			<li>Self-hosting (I have a server that I use for various stuff)</li>
 			<li>Cryptocurrency/Blockchain Technology</li>
+			<li>Drawing (still learning)</li>
 		</ul>
 	<p>
 		On this blog I mostly write about programming stuff. On <a href="https://www.artedellalettura.it/">Arte della Lettura</a> I write about the books I read (in italian).
